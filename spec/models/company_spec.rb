2cc2241f39768fdbdd95ca631aadc10045bc189c
Igor Melao <igormelao@gmail.com>
2017-05-17 19:46:53 -0300

    Created model test for Company

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,10 @@
+require_relative './../spec_helper.rb'
+
+RSpec.describe Company, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Company.new(name: FFaker::Name.name)).to be_valid
+  end
+  it "is not valid without a name" do
+    expect(Company.new()).to_not be_valid
+  end
+end
