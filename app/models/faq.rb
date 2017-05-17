3e47002141f8f59c68ab7170c0485c7cafaf9d38
Igor Melao <igormelao@gmail.com>
2017-05-17 20:19:05 -0300

    Implemented a model test for Faq

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,5 @@
 class Faq < ActiveRecord::Base
-  validates_presence_of :question, :answer
+  validates_presence_of :question, :answer, :company
 
   has_many :faq_hashtags
   has_many :hashtags, through: :faq_hashtags
---
+++ spec/models/faq_spec.rb
@@ -0,0 +1,23 @@
+require_relative "./../spec_helper.rb"
+
+RSpec.describe Faq, :type => :model do
+  before do
+    @company = Company.new(name: FFaker::Name.name)
+    @question = FFaker::Lorem.sentence
+    @answer = FFaker::Lorem.sentence
+    @faq = Faq.new(company: @company,question: @question, answer: @answer)
+  end
+
+  it "is valid with valid attributes"do
+    expect(@faq).to be_valid
+  end
+
+  it "is not valid without a question" do
+    @faq.question = nil
+    expect(@faq).to_not be_valid
+  end
+  it "is not valid without a answer" do
+    @faq.answer = nil
+    expect(@faq).to_not be_valid
+  end
+end
