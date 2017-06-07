a1ceff05624a7e7a53fdb12c6355bf429eebfe2b
Igor Melão <igormelao@gmail.com>
2017-06-07 16:26:54 -0300

    complete functionality of list faqs and yours respect tests

--- app/services/faq/list_service.rb
+++ app/services/faq/list_service.rb
@@ -2,10 +2,24 @@ module FaqModule
   class ListService
     def initialize(params,action)
       @company = Company.last
+      @action = action
+      @query = params["query"]
     end
 
     def call
-      faqs = Company.last.faqs
+      if @action == "search"
+        faqs = Faq.search(@query).where(company: @company)
+      elsif @action == "search_by_hashtag"
+        faqs = []
+        @company.faqs.each do |faq|
+          faq.hashtags.each do |hashtag|
+            faqs << faq if hashtag.name == @query
+          end
+        end
+      else
+        faqs = @company.faqs
+      end
+
       response = "*Perguntas e Respostas* \n\n"
       faqs.each do |f|
         response += "*#{ f.id }* - "
@@ -16,7 +30,6 @@ module FaqModule
         end
         response += "\n\n"
       end
-
       (faqs.count > 0) ? response : "Nada encontrado"
     end
   end
--- spec/services/faq/list_service_spec.rb
+++ spec/services/faq/list_service_spec.rb
@@ -26,10 +26,48 @@ describe FaqModule::ListService do
     end
 
     it "with search command and empty query, return dont' find message" do
-      listService = FaqModule::ListService.new({'queyr' => ''}, 'search')
+      listService = FaqModule::ListService.new({'query' => ''}, 'search')
 
       response = listService.call()
       expect(response).to match("Nada encontrado")
+    end
+
+    it "with search command and valid query, return question and answer searched in response" do
+      faq = create(:faq, company: @company)
+
+      listService = FaqModule::ListService.new({'query' => faq.question.split(" ").sample}, 'search')
+
+      response = listService.call()
+
+      expect(response).to match(faq.question)
+      expect(response).to match(faq.answer)
+    end
+
+    it "with search_by_hashtag command and invalid hashtag, return don't find message" do
+      listService = FaqModule::ListService.new({'query' => ''}, 'search_by_hashtag')
+
+      response = listService.call()
+
+      expect(response).to match("Nada encontrado")
+    end
+
+    it "with search_by_hashtag command and valid hashtag, return question and answer searched in response" do
+      faq = create(:faq, company: @company)
+      hashtag = create(:hashtag, company: @company)
+      create(:faq_hashtag, faq: faq, hashtag: hashtag)
+      faq2 = create(:faq, question: "Test", company: @company)
+
+      listService = FaqModule::ListService.new({'query' => hashtag.name}, 'search_by_hashtag')
+
+      response = listService.call()
+
+      expect(response).to match(faq.question)
+      expect(response).to match(faq.answer)
+      expect(response).not_to match(faq2.question)
+    end
+
+    it "" do
+      
     end
   end
 end
