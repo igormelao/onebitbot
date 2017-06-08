d1453a2a1d141c9370047d712979a65b6b4fecbb
Igor Melão <igormelao@gmail.com>
2017-06-08 11:15:30 -0300

    Implemented tests for app routes

--- app.rb
+++ app.rb
@@ -11,4 +11,21 @@ class App < Sinatra::Base
   get '/' do
     'Hello world!'
   end
+
+  post '/webhook' do
+    result = JSON.parse(request.body.read)["result"]
+
+    if result["contexts"].present?
+      response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
+    else
+      response = InterpretService.call(result["action"], result["parameters"])
+    end
+
+    content_type :JSON
+    {
+      "speech": response,
+      "displayText": response,
+      "source": "Slack"
+    }.to_json
+  end
 end
---
+++ spec/app_spec.rb
@@ -0,0 +1,8 @@
+require_relative './spec_helper.rb'
+
+RSpec.describe App do
+  it 'Process the API.AI request' do
+    get '/sinatra'
+    expect(last_response).to be_ok
+  end
+end
